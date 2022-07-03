d694cf51fa5d502c8dc50f81f22063b7849d8088
alvaro <alvaro.guillenc1612@gmail.com>
2022-07-03 11:24:44 -0500

    CambiosParaEntrega

--- app/src/main/java/com/example/pizzadelivery/LoginActivity.java
+++ app/src/main/java/com/example/pizzadelivery/LoginActivity.java
@@ -3,19 +3,26 @@ package com.example.pizzadelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.pizzadelivery.utilidades.Utilidades;
 import com.example.pizzadelivery.view.ContainerActivity;
 import com.example.pizzadelivery.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    ConexionSQLiteHelper conn;
+    EditText campoUser, campoPass;
 
     public static final String TAG = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        conn = new ConexionSQLiteHelper(this, "bd_pizzas", null,1);
         super.onCreate(savedInstanceState);
         try {
             setContentView(R.layout.activity_login);
@@ -23,6 +30,8 @@ public class LoginActivity extends AppCompatActivity {
             Log.e(TAG,"onCreateView", e);
             throw e;
         }
+        campoUser = (EditText) findViewById(R.id.login_username);
+        campoPass = (EditText) findViewById(R.id.login_password);
 
     }
 
@@ -30,9 +39,33 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
     }
+    public boolean test() {
+        SQLiteDatabase db = conn.getReadableDatabase();
+        String[] params = {campoUser.getText().toString()};
+        String[] campos = {Utilidades.CAMPO_PASSWORD};
+        boolean res = false;
+        try {
+            Cursor cursor = db
+                    .query(Utilidades.TABLA_USUARIO, campos, Utilidades.CAMPO_USUARIO + "=?", params, null, null, null);
+            cursor.moveToFirst();
+            if (campoPass.getText().toString().trim().equals(cursor.getString(0).toString().trim())) {
+                res = true;
+            }
+            cursor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
 
+            Toast.makeText(getApplicationContext(), "El documento no existe", Toast.LENGTH_LONG).show();
+        }
+
+        db.close();
+        return res;
+    }
     public void goContainer(View view) {
-        Intent intent = new Intent(this, CrudActivity.class);
-        startActivity(intent);
+        if (test()) {
+            Intent intent = new Intent(this, CrudActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/com/example/pizzadelivery/view/ContainerActivity.java
+++ app/src/main/java/com/example/pizzadelivery/view/ContainerActivity.java
@@ -6,20 +6,27 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.pizzadelivery.ConexionSQLiteHelper;
 import com.example.pizzadelivery.R;
 import com.example.pizzadelivery.adapter.PictureAdapterRecyclerView;
 import com.example.pizzadelivery.model.Pizza;
+import com.example.pizzadelivery.utilidades.Utilidades;
 
 import java.util.ArrayList;
 
 public class ContainerActivity extends AppCompatActivity {
     private ArrayList<Pizza> pizzas;
     private RecyclerView recyclerView;
-
+    ConexionSQLiteHelper conn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        conn = new ConexionSQLiteHelper(this, "bd_pizzas", null,1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
         recyclerView = findViewById(R.id.pictureRecycler);
@@ -38,14 +45,28 @@ public class ContainerActivity extends AppCompatActivity {
     }
 
     private void setPizzaInfo() {
-        Pizza pizza0 = new Pizza(R.drawable.piza0,"Cheese", "26");
-        pizzas.add(pizza0);
-        Pizza pizza1 = new Pizza(R.drawable.piza1,"Americana", "28");
-        pizzas.add(pizza1);
-        Pizza pizza2 = new Pizza(R.drawable.piza2,"Hawaiana", "28");
-        pizzas.add(pizza2);
-        Pizza pizza3 = new Pizza(R.drawable.piza3,"Suprema", "30");
-        pizzas.add(pizza3);
+        SQLiteDatabase db = conn.getReadableDatabase();
+        ArrayList<Integer> images = new ArrayList<Integer>();
+        images.add(R.drawable.piza0);
+        images.add(R.drawable.piza1);
+        images.add(R.drawable.piza2);
+        images.add(R.drawable.piza3);
+        try {
+            Cursor cursor = db
+                    .rawQuery("SELECT * FROM "+Utilidades.TABLA_PIZZA, null);
+            if (cursor.moveToFirst()) {
+                do {
+                    Integer img =cursor.getInt(0)-1;
+                    Pizza temp = new Pizza(images.get(img),cursor.getString(1),cursor.getDouble(2)+"");
+                    pizzas.add(temp);
+                } while (cursor.moveToNext());
+                cursor.close();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this,"El documento no existe", Toast.LENGTH_LONG).show();
+        }
+
+        db.close();
     }
     public void showToolbar(String title, boolean upButton) {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
